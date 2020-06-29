489e1978ccb1cf8278d2bb99d7011c620acc2d95
yurius564 <34649408+yurius564@users.noreply.github.com>
2020-06-29 02:42:36 -0300

    v2 start

---
+++ v2/main.js
@@ -0,0 +1,107 @@
+const screenSizeX = 6400;
+const screenSizeY = 3200;
+
+var gameInterval;
+
+var enemies = [];
+var enemyDelay = 500;
+
+var points = 3;
+var score = 0;
+
+
+
+function resetgame(){
+  
+  clearInterval(gameInterval);
+  
+  points = 3;
+  enemies = [];
+
+  background("black");
+  $("#points").html("");
+  $(canvas).unbind("click");
+
+  run();
+}
+
+
+function run(){
+  enemies = [];
+  points  = 3;
+  score   = 0;
+
+  var player  = new Polygon(3200,1600, 100, 3, 0, "white", 15, "#aaaaaa");
+  player.setBorders(0,0,screenSizeX,screenSizeY);
+  var control = new Control();
+  control.start();
+  
+  background();
+  var lastSeemPoints = 3;
+
+  var enemySpawn = setInterval(newEnemy, enemyDelay, enemies);
+
+  gameInterval = setInterval(function(){
+    background();
+
+    if(lastSeemPoints != points){
+      lastSeemPoints = points;
+      player.sides = points;
+
+      for(var e in enemies){
+        if(enemies[e].sides == points) enemies[e].lineColor = "#fcba03";
+        if(enemies[e].sides < points)  enemies[e].lineColor = "#638cc9";
+      }
+    }
+    
+    player.draw();
+
+    for(var e in enemies){
+      if(enemies[e].dead) continue;
+      enemies[e].draw();
+      var collision = player.collides(enemies[e]);
+      if(collision){
+        if(collision ==  2){
+          enemies[e].kill();
+          points++;
+          score+=20;
+        }
+        else if(collision == 1){
+          enemies[e].kill();
+          score++;
+        }
+        else if(collision == -1){
+          player.kill();
+          clearInterval(enemySpawn);
+          drawPoints();
+          $(canvas).click(resetgame);
+        }
+      }
+    }
+
+    for(var c in control.keysPressed){
+      switch(control.keysPressed[c]){
+        case 87: // W
+          player.pullTo(0,-20);
+          break;
+        case 65: // A
+          player.pullTo(-20,0);
+          break;
+        case 83: // S
+          player.pullTo(0,20);
+          break;
+        case 68: // D
+          player.pullTo(20,0);
+          break;
+      }
+    }
+  }, 1000/60);
+}
+
+
+$(document).ready(function(){
+  $(canvas).attr("width", screenSizeX);
+  $(canvas).attr("height", screenSizeY);
+
+  run();
+});
---
+++ v2/toolbox.js
@@ -0,0 +1,98 @@
+const canvas = $("#canvas")[0];
+const context = canvas.getContext("2d");
+
+var backatt = 0;
+
+
+function background(color="#070011"){
+  if(backatt++ == 0) context.globalAlpha = 0.25;
+  else{
+    context.globalAlpha = 0.5;
+    backatt = 0;
+  }
+  
+  context.globalCompositeOperation = "destination-out"
+  context.fillStyle = color;
+  context.fillRect(0,0,screenSizeX,screenSizeY);
+  context.globalCompositeOperation = "source-over"
+  context.globalAlpha = 1.0;
+}
+
+
+function drawPoly(obj){
+  context.translate(obj.x,obj.y);
+
+  context.rotate(obj.angle * Math.PI / 180);
+  context.beginPath();
+  context.moveTo (obj.size * Math.cos(0), obj.size *  Math.sin(0));          
+
+  for (var i = 1; i <= obj.sides+1;i += 1) {
+    context.lineTo (obj.size * Math.cos(i%obj.sides * 2 * Math.PI / obj.sides), obj.size * Math.sin(i%obj.sides * 2 * Math.PI / obj.sides));
+  }
+
+  context.strokeStyle = obj.lineColor;
+  context.fillStyle = obj.fillColor;
+  context.lineWidth = obj.lineWidth;
+  context.fill();
+  context.stroke();
+  context.rotate(-obj.angle * Math.PI / 180);
+
+  context.translate(-obj.size/2,-obj.size/2);
+  context.drawImage(obj.image,0,0,100,100,0,0,obj.size,obj.size);
+  context.translate(obj.size/2,obj.size/2);
+
+
+  context.translate(-obj.x,-obj.y);
+}
+
+function drawPolyRadial(obj, range=200){
+  context.translate(obj.x,obj.y);
+
+  context.rotate(obj.angle * Math.PI / 180);
+  context.beginPath();
+  context.moveTo ((obj.size+range) * Math.cos(0), (obj.size+range) *  Math.sin(0));          
+
+  for (var i = 1; i <= obj.sides+1;i += 1) {
+    context.lineTo ((obj.size+range) * Math.cos(i%obj.sides * 2 * Math.PI / obj.sides), (obj.size+range) * Math.sin(i%obj.sides * 2 * Math.PI / obj.sides));
+  }
+
+  context.strokeStyle = obj.lineColor;
+  context.lineWidth = obj.lineWidth;
+  context.stroke();
+  context.rotate(-obj.angle * Math.PI / 180);
+
+  context.translate(-obj.x,-obj.y);
+}
+
+
+function drawPoints(size=4){
+  points = String(parseFloat(points));
+  while(points.length < size){
+    points = "0"+points;
+  }
+  score = String(parseFloat(score));
+  while(score.length < size){
+    score = "0"+score;
+  }
+  $("#points").html(`SIDES: ${points}<br>SCORE: ${score}<br><br>Click to reset`);
+};
+
+
+function newEnemy(enemies){
+  var middle = points;
+  var posX  = Math.floor(Math.random() * (screenSizeX - 150) + 150);
+  var posY  = Math.floor(Math.random() * (screenSizeY - 150) + 150);
+
+  var sides = Math.floor(Math.random() * (middle + 2) + (middle - 2));
+  sides = sides < 3? 3: sides;
+  enemies.push(new Polygon(posX,posY,150,sides,0,"white",15,"red",2));
+
+  var forceX =  Math.floor(Math.random() * 40 - 20);
+  var forceY =  Math.floor(Math.random() * 40 - 20);
+
+  if(enemies[enemies.length-1].sides == points) enemies[enemies.length-1].lineColor = "#fcba03";
+  if(enemies[enemies.length-1].sides < points)  enemies[enemies.length-1].lineColor = "#638cc9";
+
+  enemies[enemies.length-1].setBorders(0,0,screenSizeX,screenSizeY);
+  enemies[enemies.length-1].applyForce(forceX, forceY);
+}
